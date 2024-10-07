0c70d465894e13a7c8f4475b2c152a53870a551e
Andi Baumann <andib7@hotmail.com>
2024-10-07 18:55:49 +0200

    Setup mongo DB

---
+++ privacypolicyservice/src/main/java/org/example/privacypolicyservice/PrivacypolicyserviceApplication.java
@@ -0,0 +1,13 @@
+package org.example.privacypolicyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivacypolicyserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivacypolicyserviceApplication.class, args);
+	}
+
+}
