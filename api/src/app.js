f9824ac5ae9feaa339f9dde6aa3ce219d21170ac
Andi Baumann <andib7@hotmail.com>
2024-11-16 11:19:54 +0100

    API description

--- api/src/app.js
+++ api/src/app.js
@@ -15,6 +15,14 @@ app.use(bodyParser.json());
 
 
 // Define routes
+// 4 Incoming routes
+// - objectDetection (dataservice_url triggers matchingservice with POST http://localhost:8082/api/analyze-device "dataservice_url")
+// - getActions (get all actions from api service)
+// - executeActions (action_id, dataservice_url, with interaction iot device POST...)
+//      - getPrivacyAction (sends json action ID and gets request for IOT device from AI Service)
+// - checkActionStatus (GET from hololens to api service to check action status)
+
+
 app.use('/api/output', outputRoutes);
 app.use('/api/input', inputRoutes);
 //app.use('/api/iot', iotRoutes);
