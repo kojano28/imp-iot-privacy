d0c5d7d4efbe0ec35016c12252d0981dce802373
Andi Baumann <andib7@hotmail.com>
2025-01-17 19:51:02 +0100

    readme global

--- privacypolicyservice/src/services/nlpService.js
+++
@@ -1,9 +0,0 @@
-// src/routes/DataRoutes.js
-const express = require('express');
-const router = express.Router();
-const privacyPolicyController = require('../controllers/privacyPolicyController');
-
-// Route to get privacy policy by device name and analyze it
-router.get('/privacy-policy', privacyPolicyController.getPrivacyPolicyByDeviceName);
-
-module.exports = router;
